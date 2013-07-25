02b883a8ab146bbe4e0d6655fd78a809940dadbd
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-07-25 17:52:03 +0200

    initial commit

---
+++ js/impress-substeps.js
@@ -0,0 +1,41 @@
+
+var substep = document.querySelector( ".substep" );
+for (var i = 0; i < subteps.length; i++) {
+  substeps[i].classList.add('hidden');
+}
+
+// każdy substep na początku ma klasę 'hidden'
+
+function substepNext() {
+  
+    var activeStep = document.querySelector( ".step.active" );
+  
+    var substeps = activeStep.querySelectorAll( ".substep" );
+  
+    var hiddenSubsteps = ....;
+    
+    
+    [ a, b, c, d ]
+
+    // jeśli nie ma substepów to    
+    if (hiddenSubsteps.length == 0) {
+      impress().next();
+    } else {
+      // pokaż substep
+      // nadaj my klasę 'visible'
+      var substep = ??;
+      substep.classList.remove("hidden");
+      substep.classList.add('visible')
+    }
+}
+  
+function substepPrev() {
+
+  impress().prev();
+
+}
+
+
+
+
+
