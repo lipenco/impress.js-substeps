3ce511b11b7259b70943794ecd97d1483d113b1b
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-07-27 20:13:55 +0200

    substpes don't work

--- js/substeps.js
+++ js/substeps.js
@@ -1,36 +1,38 @@
 
-var substep = document.querySelector( ".substep" );
-for (var i = 0; i < subteps.length; i++) {
-  substeps[i].classList.add('hidden');
+function substeps() {
+		var substeps = document.querySelector( ".substep" );
+		for (var i = 0; i < subteps.length; i++) {
+		  substeps[i].classList.add('hidden');
+		}
 }
 
 // każdy substep na początku ma klasę 'hidden'
 
-function substepNext() {
+// function substepNext() {
   
-    var activeStep = document.querySelector( ".step.active" );
+//     var activeStep = document.querySelector( ".step.active" );
   
-    var substeps = activeStep.querySelectorAll( ".substep" );
+//     var substeps = activeStep.querySelectorAll( ".substep" );
   
-   var hiddenSubsteps = activeStep.querySelectorAll( ".hidden" );
+//    var hiddenSubsteps = activeStep.querySelectorAll( ".hidden" );
 
    
     
-   [ a, b, c, d ]
+//    [ a, b, c, d ]
 
-    // jeśli nie ma substepów to    
-    if (hiddenSubsteps.length == 0) {
-      impress().next();
-    } else {
-     for (var i = 0; i < hiddenSubsteps.length; i++)
-      var substep = hiddenSubsteps[i];
-      substep.classList.remove("hidden");
-      substep.classList.add('visible');
-   }
-}
+//     // jeśli nie ma substepów to    
+//     if (hiddenSubsteps.length == 0) {
+//       impress().next();
+//     } else {
+//      for (var i = 0; i < hiddenSubsteps.length; i++)
+//       var substep = hiddenSubsteps[i];
+//       substep.classList.remove("hidden");
+//       substep.classList.add('visible');
+//    }
+// }
   
-function substepPrev() {
+// function substepPrev() {
 
- impress().prev();
+//  impress().prev();
 
-}
+// }
