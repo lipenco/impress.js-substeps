89bb6e2727042c4ad05cb42568b03257aa6df36f
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-07-29 14:28:28 +0200

    1th step

--- js/substeps.js
+++ js/substeps.js
@@ -1,47 +1,20 @@
 var substeps = document.querySelectorAll( ".substep" );
 for (i = 0 ; i < substeps.length; i++ ) {
   substeps[i].classList.add("hidden");
-}
+};
  
- // var showSubsteps = $( ".substep.hidden" );
- //  $( ".arrows" ).on( "click", function( event ) {
- //   showSubsteps.show();
- //    }); 
 
 function substepNext() {
 var activeStep = document.querySelector( ".step.active" );
-var substeps = activeStep.querySelectorAll( ".substep" );
-var hiddenSubsteps = activeStep.querySelectorAll( ".substep.hidden" );
-		for (i = 0 ; i < hiddenSubsteps.length; i++ ) {
-		  if (hiddenSubsteps.length == 0) {
-		      impress.next();
-		    } 
-		  else 
-		 {
-		    hiddenSubsteps.classList.remove("hidden");
-		    substep.classList.add('visible');
-		 }
-}
-
-var el = document.getElementById("arrow");
-el.addEventListener("click", substepNext, false);
-
-// function substepNext() {
-//  
-//     
-              
-//    }   
-// }
+var substeps = activeStep.querySelectorAll(".substep.hidden");
+	for (i = 0 ; i < substeps.length; i++ ) {
+		if (substeps.length == 0) {
+		      impress().next();
+		   } 
+		else {
+		   substeps[i].classList.add("visible");
+		   }
+	}
+};
 
 
-    // hiddenSubsteps.nextSibling().classList.remove("hidden");
-    //    hiddenSubsteps.nextSibling().classList.add('visible');
-
-//       // show substep
-//       // add class 'visible'
- //   var substep = hiddenSubsteps[i];
- //      substep.classList.remove("hidden");
- //      substep.classList.add('visible');
- //     }
- // }
-  
