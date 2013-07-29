57b87b3eec9dc801260ae0ae703426bc5dc5aa32
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-07-29 23:52:50 +0200

    refactioring

--- js/substeps.js
+++ js/substeps.js
@@ -2,25 +2,6 @@ var substepsAll = document.querySelectorAll(".substep");
 for (i = 0; i < substepsAll.length; i++) {
     substepsAll[i].classList.add("hidden");
 }
-<<<<<<< HEAD
-
-
-function substepNext() {
-    var activeStep = document.querySelector(".step.active");
-    var substeps = activeStep.querySelectorAll(".substep");
-    var hidden = activeStep.querySelector(".substep.hidden");
-
-    if (!hidden) {
-        impress().next();
-        return 0;
-    } else {
-        hidden.classList.add("visible");
-        hidden.classList.remove("hidden");
-    }
-    if (substeps.length == 0) {
-        impress().next();
-    }
-=======
 
 
 function substepNext() {
@@ -37,19 +18,6 @@ function substepNext() {
     }
 }
 
-function substepPrev() {
-    var activeStep = document.querySelector(".step.active");
-    var substeps = activeStep.querySelectorAll(".substep");
-    var visible = activeStep.querySelectorAll(".substep.visible");
-    if (visible.length == 0) {
-        impress().prev();
-    } else {
-        visible[visible.length-1].classList.remove("visible");
-        visible[visible.length-1].classList.add("hidden");
-    }
-    
->>>>>>> master
-}
 
 function substepPrev() {
     var activeStep = document.querySelector(".step.active");
