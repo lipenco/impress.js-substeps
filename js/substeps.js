3211a33610a957ddc5c710d8001032e7c36f3fcc
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-07-29 10:59:28 +0200

    try

--- js/substeps.js
+++ js/substeps.js
@@ -2,22 +2,33 @@ var substeps = document.querySelectorAll( ".substep" );
 for (i = 0 ; i < substeps.length; i++ ) {
   substeps[i].classList.add("hidden");
 }
+ 
+ // var showSubsteps = $( ".substep.hidden" );
+ //  $( ".arrows" ).on( "click", function( event ) {
+ //   showSubsteps.show();
+ //    }); 
 
-var hiddenSubsteps = $( ".hidden" );
-    $( ".arrows" ).on( "click", function( event ) {
-      hiddenSubsteps.show();
-     }); 
+function substepNext() {
+var activeStep = document.querySelector( ".step.active" );
+var substeps = activeStep.querySelectorAll( ".substep" );
+var hiddenSubsteps = activeStep.querySelectorAll( ".substep.hidden" );
+		for (i = 0 ; i < hiddenSubsteps.length; i++ ) {
+		  if (hiddenSubsteps.length == 0) {
+		      impress.next();
+		    } 
+		  else 
+		 {
+		    hiddenSubsteps.classList.remove("hidden");
+		    substep.classList.add('visible');
+		 }
+}
 
+var el = document.getElementById("arrow");
+el.addEventListener("click", substepNext, false);
 
 // function substepNext() {
-//  var activeStep = document.querySelector( ".step.active" );
-//  var substeps = activeStep.querySelectorAll( ".substep" );
-//  var hiddenSubsteps = activeStep.querySelectorAll( ".substep.hidden" );
-//     if (hiddenSubsteps.length == 0) {
-//       impress().next();
-//     } 
-//     else 
-//     {
+//  
+//     
               
 //    }   
 // }
