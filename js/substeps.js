392835f8a1bb586ecf21cb7180b1735019f0c84b
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-07-29 14:36:42 +0200

    first draft

--- js/substeps.js
+++ js/substeps.js
@@ -1,20 +1,21 @@
-var substeps = document.querySelectorAll( ".substep" );
-for (i = 0 ; i < substeps.length; i++ ) {
-  substeps[i].classList.add("hidden");
+var substepsAll = document.querySelectorAll( ".substep" );
+for (i = 0 ; i < substepsAll.length; i++ ) {
+  substepsAll[i].classList.add("hidden");
 };
  
 
 function substepNext() {
 var activeStep = document.querySelector( ".step.active" );
 var substeps = activeStep.querySelectorAll(".substep.hidden");
-	for (i = 0 ; i < substeps.length; i++ ) {
-		if (substeps.length == 0) {
+	for (i = 0 ; i < substeps.length; i++) {
+		if (substeps.length = 0) {
 		      impress().next();
 		   } 
 		else {
 		   substeps[i].classList.add("visible");
+		   substeps[i].classList.remove("hidden");
 		   }
-	}
+		}
 };
 
 
