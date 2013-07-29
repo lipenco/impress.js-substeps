140086fb0a897793c736194828fff727b6b99f74
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-07-29 22:48:12 +0200

    beautyfy

--- js/substeps.js
+++ js/substeps.js
@@ -1,41 +1,39 @@
 var substepsAll = document.querySelectorAll(".substep");
-for (i = 0 ; i < substepsAll.length; i++) {
-  substepsAll[i].classList.add("hidden");
+for (i = 0; i < substepsAll.length; i++) {
+    substepsAll[i].classList.add("hidden");
 }
- 
+
 
 function substepNext() {
-var activeStep = document.querySelector(".step.active");
-var substeps = activeStep.querySelectorAll(".substep");
-var hidden = activeStep.querySelector(".substep.hidden");
+    var activeStep = document.querySelector(".step.active");
+    var substeps = activeStep.querySelectorAll(".substep");
+    var hidden = activeStep.querySelector(".substep.hidden");
 
-     if (!hidden) {
-		     impress().next();
-		     return 0;
-		   } 
-     else {
-		   hidden.classList.add("visible");
-		   hidden.classList.remove("hidden");
-		   }
-	 if (substeps.length == 0){
-       impress().next();
-	 }	
+    if (!hidden) {
+        impress().next();
+        return 0;
+    } else {
+        hidden.classList.add("visible");
+        hidden.classList.remove("hidden");
+    }
+    if (substeps.length == 0) {
+        impress().next();
+    }
 }
 
 function substepPrev() {
-var activeStep = document.querySelector(".step.active");
-var substeps = activeStep.querySelectorAll(".substep");
-var visible = activeStep.querySelector(".substep.visible");
+    var activeStep = document.querySelector(".step.active");
+    var substeps = activeStep.querySelectorAll(".substep");
+    var visible = activeStep.querySelector(".substep.visible");
 
-		  if (!visible) {
-		     impress().prev();
-		     return 0;
-		   } 
-		else {
-		   visible.classList.remove("visible");
-		   visible.classList.add("hidden");
-		   }
-	if (substeps.length == 0) {
-		impress().prev();
-		}
+    if (!visible) {
+        impress().prev();
+        return 0;
+    } else {
+        visible.classList.remove("visible");
+        visible.classList.add("hidden");
+    }
+    if (substeps.length == 0) {
+        impress().prev();
+    }
 }
