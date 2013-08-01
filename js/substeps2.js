8816e8dfb4a132ccf4ad5184c2f6a0d36331ef1f
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-01 09:41:18 +0200

    present and past

--- js/substeps2.js
+++ js/substeps2.js
@@ -1,20 +1,20 @@
 var substepsAll = document.querySelectorAll(".substep");
 for (i = 0; i < substepsAll.length; i++) {
-    substepsAll[i].classList.add("hidden");
+    substepsAll[i].classList.add("future");
 }
 
 function substepNext() {
     var activeStep = document.querySelector(".step.active");
     var substeps = activeStep.querySelectorAll(".substep");
-    var future = activeStep.querySelectorAll(".substep.hidden");
-    var present = activeStep.querySelectorAll(".substep.visible");
+    var future = activeStep.querySelectorAll(".substep.future");
+    var present = activeStep.querySelectorAll(".substep.present");
 
     if (future.length == 0) {
         impress().next();
        
     } else {
         future[0] = present[0];
-        future[0].classList.remove("hidden");
+        future[0].classList.remove("future");
     }
 }
 
