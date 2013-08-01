28a963644fe2096839fd9211e9a2aeb7aa922945
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-01 10:17:07 +0200

    adding present furure past

--- js/substeps2.js
+++ js/substeps2.js
@@ -6,15 +6,17 @@ for (i = 0; i < substepsAll.length; i++) {
 function substepNext() {
     var activeStep = document.querySelector(".step.active");
     var substeps = activeStep.querySelectorAll(".substep");
-    var future = activeStep.querySelectorAll(".substep.future");
-    var present = activeStep.querySelectorAll(".substep.present");
+    var future = activeStep.querySelectorAll(".future");
+    var present = activeStep.querySelectorAll(".present");
 
     if (future.length == 0) {
         impress().next();
        
     } else {
-        future[0] = present[0];
+        future[0].classList.add("present");
         future[0].classList.remove("future");
+        present[0].classList.add("past");
+        present[0].classList.remove("present");
     }
 }
 
