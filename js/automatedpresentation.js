c7f3d43522dd849c1bfe8216f921febed15cc633
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-16 21:26:06 +0400

    it is working , but with a bug

--- js/automatedpresentation.js
+++ js/automatedpresentation.js
@@ -1,21 +1,18 @@
 
 function automatedPresentationWithSubsteps() {
-    var element = document.querySelector(".substep");
-    // element.addEventListener("keyup", function(e) {
-    document.addEventListener("impress()", function (e) {
-        var duration = e.target.dataset.duration;
+    var elements = document.querySelector(".substep");
+// for (var index = 0; index < elements.length; index++) {
+     document.addEventListener("impress:stepenter", function () {
+        var duration = elements.dataset.duration;
         if (!duration) {
             duration = 3000;
         } 
-        var timing = setTimeout(function() {
+        var timing = setInterval(function() {
         substepNext()
         }, duration);
         }, false);
-     if (typeof timing !== "undefined") {
-        clearTimeout(timing);
-        }
+   
 }
-
 
 
 
