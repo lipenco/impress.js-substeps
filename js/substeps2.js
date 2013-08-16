7423cf66374ac2cbc238d5e90413d5113a2260de
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-08-16 23:14:58 +0400

    js beutyfy

--- js/substeps2.js
+++ js/substeps2.js
@@ -14,12 +14,13 @@ function substepNext() {
 
     if (future.length == 0) {
         impress().next();
-        present[present.length-1].classList.add("past");
-       
+        present[present.length - 1].classList.add("past");
+
     } else {
         future[0].classList.add("present");
         future[0].classList.remove("future");
-        if( present.length > 0 ) {
+        if {
+            present.length > 0) {
             present[0].classList.add("past");
             present[0].classList.remove("present");
         }
@@ -31,25 +32,25 @@ function substepNext() {
 
 function substepPrev() {
     impress().prev();
-} 
+}
 
-document.addEventListener("keyup", function ( event ) {
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
-                switch( event.keyCode ) {
-                    case 33: // pg up
-                    case 37: // left
-                    case 38: // up
-                           substepPrev() 
-                             break;
-                    case 9:  // tab
-                    case 32: // space
-                    case 34: // pg down
-                    case 39: // right
-                    case 40: // down
-                             substepNext();
-                             break;
-                }
-                
-                event.preventDefault();
-            }
-        }, false);
+document.addEventListener("keyup", function (event) {
+    if (event.keyCode === 9 || (event.keyCode >= 32 && event.keyCode <= 34) || (event.keyCode >= 37 && event.keyCode <= 40)) {
+        switch (event.keyCode) {
+        case 33: // pg up
+        case 37: // left
+        case 38: // up
+            substepPrev()
+            break;
+        case 9: // tab
+        case 32: // space
+        case 34: // pg down
+        case 39: // right
+        case 40: // down
+            substepNext();
+            break;
+        }
+
+        event.preventDefault();
+    }
+}, false);
